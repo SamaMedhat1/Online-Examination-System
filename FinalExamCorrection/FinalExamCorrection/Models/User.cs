268e1e36592ae73dd9a33c2b83f8ce816581e72f
Sama Medhat <99523498+SamaMedhat1@users.noreply.github.com>
2024-05-08 08:02:36 +0300

    Add Online-Examination-System

---
+++ FinalExamCorrection/FinalExamCorrection/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FinalExamCorrection.Models
+{
+    public class User
+    {
+        public string Email { get; set; }
+
+		[DataType(DataType.Password)]
+		public string Password { get; set; }
+        public string Id { get; set; }
+    }
+}
